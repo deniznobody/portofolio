b1bf567fb2f9ef8c96cfc8208eb76fc5c02a30ba
o <im@trapst.ar>
2023-11-30 18:49:36 +0400

    added lastfm link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,7 +47,7 @@
 			</div>
 		{:else}
 			<div class="flex flex-row items-center p-6 w-max-48  gap-x-4 mt-4 h-24 m text-text border-[1px] shadow-lg  border-[#252525] rounded-md justify-center mt-t">
-				<img class="rounded-md" src="https://kharoxe.wtf/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsong.a188f83d.webp&w=96&q=75" height="72" width="72" alt=""> 
+				<img class="rounded-md border-[2px] border-[#252525]" src="https://kharoxe.wtf/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsong.a188f83d.webp&w=96&q=75" height="72" width="72" alt=""> 
 				<div class="flex flex-col">
 					Not listening to anything.
 					<div class="flex flex-row gap-x-2 text-[#a8a8a8] text-sm">
@@ -103,6 +103,13 @@
 					d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
 				/><path d="M9 18c-4.51 2-5-2-7-2" /></svg
 			>
+		</a>
+		<a
+			href="https://www.last.fm/user/deniznobody"
+			target="_blank"
+			class="hover:cursor-pointer hover:text-primary transition-all duration-300"
+		>
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><path fill="currentColor" d="m225.8 367.1l-18.8-51s-30.5 34-76.2 34c-40.5 0-69.2-35.2-69.2-91.5c0-72.1 36.4-97.9 72.1-97.9c66.5 0 74.8 53.3 100.9 134.9c18.8 56.9 54 102.6 155.4 102.6c72.7 0 122-22.3 122-80.9c0-72.9-62.7-80.6-115-92.1c-25.8-5.9-33.4-16.4-33.4-34c0-19.9 15.8-31.7 41.6-31.7c28.2 0 43.4 10.6 45.7 35.8l58.6-7c-4.7-52.8-41.1-74.5-100.9-74.5c-52.8 0-104.4 19.9-104.4 83.9c0 39.9 19.4 65.1 68 76.8c44.9 10.6 79.8 13.8 79.8 45.7c0 21.7-21.1 30.5-61 30.5c-59.2 0-83.9-31.1-97.9-73.9c-32-96.8-43.6-163-161.3-163C45.7 113.8 0 168.3 0 261c0 89.1 45.7 137.2 127.9 137.2c66.2 0 97.9-31.1 97.9-31.1z"/></svg>
 		</a>
 	</div>
 	<div class="text-primary mt-2 hover:underline hover:cursor-pointer">
