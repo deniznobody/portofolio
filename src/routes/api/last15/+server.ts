2054151a8d40e74b643e5496b7f1a3ba5575dd46
dêniz <im@trapst.ar>
2023-12-25 03:36:55 +0400

    make api secure

--- src/routes/api/last15/+server.ts
+++ src/routes/api/last15/+server.ts
@@ -1,8 +1,7 @@
 export async function GET({}) {
     let username = "deniznobody";
-    let api_key = "cdf135c833971d694c742d2384c1bbd3";
 
-    const res = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${username}&api_key=${api_key}&format=json&nowplaying=true&limit=15`);
+    const res = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${username}&api_key=${process.env.api}&format=json&nowplaying=true&limit=15`);
 
     const response = await res.json()
     const tracks = response.recenttracks;
